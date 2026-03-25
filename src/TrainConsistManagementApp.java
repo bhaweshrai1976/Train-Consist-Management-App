9b3db1c23113190cb9a1dbf51ed372d54c32350d
your GitHub username <your GitHub email>
2026-03-25 10:27:44 +0530

    UC2

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,16 +1,48 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class TrainConsistManagementApp {
+
+public class TrainConsistManagementApp
+{
 
     public static void main(String[] args) {
 
-        // Display welcome banner
-        System.out.println("\n==============================");
-        System.out.println(" Train Consist Management App");
-        System.out.println("==============================\n");
+        System.out.println("======================================");
+        System.out.println(" UC2 - Add Passenger Bogies to Train ");
+        System.out.println("======================================\n");
 
-        // Create a dynamic list to store train bogies
-        List<String> trainConsist = new ArrayList<>();
+
+        List<String> passengerBogies = new ArrayList<>();
+
+
+        passengerBogies.add("B1");
+        passengerBogies.add("B2");
+        passengerBogies.add("B3");
+
+
+        System.out.println("Initial Train Consist:");
+        for (String bogie : passengerBogies) {
+            System.out.println(bogie);
+        }
+
+
+        passengerBogies.set(1, "B2-Updated");
+
+
+        passengerBogies.remove("B3");
+
+
+        System.out.println("\nChecking availability of B1...");
+        if (passengerBogies.contains("B1")) {
+            System.out.println("B1 is available in the train.");
+        } else {
+            System.out.println("B1 is NOT available in the train.");
+        }
+
+
+        System.out.println("\nFinal Train Consist:");
+        for (String bogie : passengerBogies) {
+            System.out.println(bogie);
+        }
     }
 }
