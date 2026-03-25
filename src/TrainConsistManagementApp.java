baf96df61e533bae45625a2c096a9a8c6a4a8659
your GitHub username <your GitHub email>
2026-03-25 09:58:17 +0530

    UC1: Add welcome message module

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,2 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class TrainConsistManagementApp {
+
+    public static void main(String[] args) {
+
+        // Display welcome banner
+        System.out.println("\n==============================");
+        System.out.println(" Train Consist Management App");
+        System.out.println("==============================\n");
+
+        // Create a dynamic list to store train bogies
+        List<String> trainConsist = new ArrayList<>();
+    }
 }
